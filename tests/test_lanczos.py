2ea247524977387abf879c23999d0b6106deb262
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-11 17:27:05 +0200

    Proper tests for tridiagonalisation

--- tests/test_lanczos.py
+++ tests/test_lanczos.py
@@ -12,42 +12,6 @@ def A(n):
     return A.T @ A + np.eye(n)
 
 
-#
-#
-# def test_trace_fun(B):
-#
-#     key = prng.PRNGKey(seed=1)
-#     keys = prng.split(key, num=1)
-#     log_det = lanczos.trace_fun(np.log, lambda v: B @ v, m, keys=keys, shape=(n,))
-#
-#     sign, logdet = linalg.slogdet(B)
-#     assert log_det == logdet
-#
-#
-# def test_tridiagonal_sym_reorthogonalise():
-#     n = 10
-#     m = 10
-#     A = np.reshape(np.arange(1.0, n**2 + 1.0), (n, n))
-#     B = A.T @ A  # symmetrise for good measure
-#     B = B / linalg.norm(B) + np.eye(n)
-#     # B = np.eye(n)
-#
-#     key = prng.PRNGKey(seed=1)
-#     (d_m, e_m), W_m = lanczos.tridiagonal_sym_reorthogonalise(
-#         lambda v: B @ v, m, key=key, shape=(n,)
-#     )
-#     assert np.shape(W_m) == (m, n)
-#     assert np.shape(d_m) == (m,)
-#     assert np.shape(e_m) == (m - 1,)
-#
-#     T_m = W_m @ B @ W_m.T
-#     print(np.diag(T_m), d_m)
-#     print()
-#     print(np.diag(T_m, -1) - e_m)
-#     assert np.allclose(np.diag(T_m), d_m, atol=1e-6)
-#     assert np.allclose(np.diag(T_m, 1), e_m, atol=1e-5)
-
-
 def test_tridiagonal_error_for_too_high_order(A):
     n, _ = np.shape(A)
     order = n
@@ -58,7 +22,7 @@ def test_tridiagonal_error_for_too_high_order(A):
         _ = lanczos.tridiagonal(lambda v: A @ v, order, key=key, shape=(n,))
 
 
-def test_full_batch(A):
+def test_tridiagonal(A):
     """If m == n, the matrix should be equal to the full tridiagonal."""
     n, _ = np.shape(A)
     order = n - 1
