403cc01e8bf2dbb2a17656c364b3bd15b024be41
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-11 13:04:54 +0200

    Cleaned up test

--- tests/test_lanczos.py
+++ tests/test_lanczos.py
@@ -11,26 +11,14 @@ def test_tridiagonal_sym():
     # B = A.T @ A + np.eye(n)  # symmetrise for good measure
     B = np.eye(n)
 
-    key = prng.PRNGKey(seed=2)
+    key = prng.PRNGKey(seed=1)
     success, (d_m, e_m), W_m = lanczos.tridiagonal_sym(
         lambda v: v, m, key=key, shape=(n,)
     )
-    # assert np.shape(W_m) == (n, m)
-    # assert np.shape(d_m) == (m,)
-    # assert np.shape(e_m) == (m-1,)
-    print()
-    print("Success:", success)
-    print()
-    print()
+    assert np.shape(W_m) == (m, n)
+    assert np.shape(d_m) == (m,)
+    assert np.shape(e_m) == (m - 1,)
+
     T_m = W_m @ B @ W_m.T
-    print(W_m @ B @ W_m.T)
-    print()
-    print("Diag:", np.diag(T_m))
-    print("DIag:", d_m)
-
-    print()
-    print("Offdiag (via W):", np.diag(T_m, 1))
-    print("Offdiag (Lanczos):", e_m)
-
-    assert np.allclose(np.diag(T_m), d_m)
-    assert np.allclose(np.diag(T_m, 1), e_m)
+    assert np.allclose(np.diag(T_m), d_m, atol=1e-6)
+    assert np.allclose(np.diag(T_m, 1), e_m, atol=1e-6)
