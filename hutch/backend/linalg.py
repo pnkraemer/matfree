e7e308ddf4f1df89ea653980d930fa51344ef5bd
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-11 18:12:59 +0200

    Removed commented-out code

--- hutch/backend/linalg.py
+++ hutch/backend/linalg.py
@@ -10,11 +10,3 @@ det = jnp.linalg.det
 slogdet = jnp.linalg.slogdet
 tridiagonal = jax.lax.linalg.tridiagonal
 eigh_tridiagonal = jax.scipy.linalg.eigh_tridiagonal
-#
-# def eigh_tridiagonal(d, e, **kwargs):
-#     d = onp.asarray(d)
-#     e = onp.asarray(e)
-#     x, y = scipy.linalg.eigh_tridiagonal(d, e, **kwargs)
-#     x = jnp.asarray(x)
-#     y = jnp.asarray(y)
-#     return x, y
