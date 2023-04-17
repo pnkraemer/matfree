13ace1f78bac4edb9d769df29c673c4eed193606
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-17 14:30:53 +0200

    Auxiliary output

--- hutch/montecarlo.py
+++ hutch/montecarlo.py
@@ -6,7 +6,7 @@ from hutch.backend import flow, np, prng, transform
 def montecarlo(f, /, *, sample_fn):
     def f_mc(key, /):
         x = sample_fn(key)
-        return f(x)
+        return f(x), None
 
     return f_mc
 
@@ -14,8 +14,8 @@ def montecarlo(f, /, *, sample_fn):
 def mean_vmap(f, num, /):
     def g(key, /):
         subkeys = prng.split(key, num)
-        fx = transform.vmap(f)(subkeys)
-        return np.mean(fx, axis=0)
+        fx, info = transform.vmap(f)(subkeys)
+        return np.mean(fx, axis=0), info
 
     return g
 
@@ -23,8 +23,8 @@ def mean_vmap(f, num, /):
 def mean_map(f, num, /):
     def g(key, /):
         subkeys = prng.split(key, num)
-        fx = flow.map(f, subkeys)
-        return np.mean(fx, axis=0)
+        fx, info = flow.map(f, subkeys)
+        return np.mean(fx, axis=0), info
 
     return g
 
@@ -32,17 +32,19 @@ def mean_map(f, num, /):
 def mean_loop(f, num, /):
     def g(key, /):
         def body(i, mean_and_key):
-            mean, k = mean_and_key
+            mean, k, _ = mean_and_key
             _, subk = prng.split(k)
 
-            fx = f(subk)
+            fx, info = f(subk)
 
             mean_new = (mean * i + fx) / (i + 1)
-            return mean_new, subk
+            return mean_new, subk, info
 
-        init_val = (0.0, key)
-        mean, _key = flow.fori_loop(1, upper=num + 1, body_fun=body, init_val=init_val)
+        init_val = (0.0, key, None)
+        mean, _key, info = flow.fori_loop(
+            1, upper=num + 1, body_fun=body, init_val=init_val
+        )
 
-        return mean
+        return mean, info
 
     return g
--- tests/test_montecarlo.py
+++ tests/test_montecarlo.py
@@ -19,7 +19,15 @@ _ALL_MEAN_FNS = [montecarlo.mean_vmap, montecarlo.mean_map, montecarlo.mean_loop
 @testing.parametrize("mean_fn", _ALL_MEAN_FNS)
 def test_mean(f_mc, key, mean_fn):
     f_mc_mean = mean_fn(f_mc, 10_000)
-    received = f_mc_mean(key)
+    received, _info = f_mc_mean(key)
+    assert np.allclose(received, 1.0, rtol=1e-2)
+
+
+@testing.parametrize("key", [prng.PRNGKey(1)])
+@testing.parametrize("mean_fn", _ALL_MEAN_FNS)
+def test_mean_parametrised(f_mc, key, mean_fn):
+    f_mc_mean = mean_fn(f_mc, 10_000)
+    received, _info = f_mc_mean(key)
     assert np.allclose(received, 1.0, rtol=1e-2)
 
 
@@ -27,5 +35,5 @@ def test_mean(f_mc, key, mean_fn):
 @testing.parametrize("mean_fn1, mean_fn2", (_ALL_MEAN_FNS[1:], _ALL_MEAN_FNS[:-1]))
 def test_mean_nested(f_mc, key, mean_fn1, mean_fn2):
     f_mc_mean = montecarlo.mean_loop(montecarlo.mean_vmap(f_mc, 5), 10_000)
-    received = f_mc_mean(key)
+    received, _info = f_mc_mean(key)
     assert np.allclose(received, 1.0, rtol=1e-2)
