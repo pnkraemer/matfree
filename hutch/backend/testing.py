640c8abdca4a5b16dc4acae833dbffcb6221ccd8
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-21 13:16:44 +0200

    AD test (#28)

--- hutch/backend/testing.py
+++ hutch/backend/testing.py
@@ -1,8 +1,10 @@
 """Test-utilities."""
 
+import jax.test_util
 import pytest
 import pytest_cases
 
 fixture = pytest_cases.fixture
 parametrize = pytest.mark.parametrize
 raises = pytest.raises
+check_grads = jax.test_util.check_grads
