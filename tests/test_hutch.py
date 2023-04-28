a93a15ce2305b2aea7191b1a8fd59cdd81e29638
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-28 13:47:53 +0200

    Add (squared) Frobenius norm to hutch.py

--- tests/test_hutch.py
+++ tests/test_hutch.py
@@ -24,6 +24,32 @@ def fixture_key():
 @testing.parametrize("num_samples_per_batch", [1_000])
 @testing.parametrize("dim", [1, 10])
 @testing.parametrize("sample_fun", [montecarlo.normal, montecarlo.rademacher])
+def test_frobeniusnorm_squared(
+    fun, key, num_batches, num_samples_per_batch, dim, sample_fun
+):
+    """Assert that the Frobenius norm estimate is accurate."""
+    # Linearise function
+    x0 = prng.uniform(key, shape=(dim,))  # random lin. point
+    _, jvp = func.linearize(fun, x0)
+    J = func.jacfwd(fun)(x0)
+
+    # Estimate the trace
+    fun = sample_fun(shape=np.shape(x0), dtype=np.dtype(x0))
+    estimate = hutch.frobeniusnorm_squared(
+        jvp,
+        num_batches=num_batches,
+        key=key,
+        num_samples_per_batch=num_samples_per_batch,
+        sample_fun=fun,
+    )
+    truth = np.trace(J.T @ J)
+    assert np.allclose(estimate, truth, rtol=1e-2)
+
+
+@testing.parametrize("num_batches", [1_000])
+@testing.parametrize("num_samples_per_batch", [1_000])
+@testing.parametrize("dim", [1, 10])
+@testing.parametrize("sample_fun", [montecarlo.normal, montecarlo.rademacher])
 def test_trace(fun, key, num_batches, num_samples_per_batch, dim, sample_fun):
     """Assert that the estimated trace approximates the true trace accurately."""
     # Linearise function
