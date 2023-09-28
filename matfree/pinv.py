45b659f1fb366218a567d757ffb0ad943961f8c9
Nicholas Krämer <nicokraemer@gmx.net>
2023-09-28 15:59:34 +0200

    Left a few todos

--- matfree/pinv.py
+++ matfree/pinv.py
@@ -2,12 +2,15 @@
 
 from matfree.backend.typing import Callable
 
+# todo: what happens if we want to reuse e.g. LU decompositions of AA^*?
+# todo: should we not just return pinv-matvec but also pinv-vecmat?
+
 
 def pinv_tall(matvec: Callable, vecmat: Callable, *, solve: Callable) -> Callable:
-    """Moore-Penrose pseudo-inverse of a full-rank, ''tall'' matrix.
+    r"""Construct the (Moore-Penrose) pseudo-inverse of a full-rank, ''tall'' matrix.
 
-    Implemented as a left-inverse: M = A^* (A A*)^{-1}
-    which is applicable if A has full **column**-rank.
+    Implemented as a left-inverse, $A^\dagger := A^* (A A^*)^{-1}$,
+    which is applicable if $A$ has full **column**-rank.
     """
 
     def pinv(s):
@@ -17,10 +20,10 @@ def pinv_tall(matvec: Callable, vecmat: Callable, *, solve: Callable) -> Callabl
 
 
 def pinv_wide(matvec: Callable, vecmat: Callable, *, solve: Callable) -> Callable:
-    """Moore-Penrose pseudo-inverse of a full-rank, ''wide'' matrix.
+    r"""Construct the (Moore-Penrose) pseudo-inverse of a full-rank, ''wide'' matrix.
 
-    Implemented as a right-inverse: M = (A* A)^{-1} A^*
-    which is applicable if A has full **row**-rank.
+    Implemented as a right-inverse, $A^\dagger := (A^* A)^{-1} A^*$,
+    which is applicable if $A$ has full **row**-rank.
     """
 
     def pinv(s):
