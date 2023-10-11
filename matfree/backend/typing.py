9ad2d963c4b6bb4338fab9b143a4534cb51a5dd1
Nicholas Krämer <nicokraemer@gmx.net>
2023-10-11 09:05:36 +0200

    Bundled all meta-information in pyproject.toml (#154)
    * Bundled all meta-information in pyproject.toml
    
    * Removed python 3.11 claim

--- matfree/backend/typing.py
+++ matfree/backend/typing.py
@@ -1,8 +1,7 @@
 """Types."""
 # fmt: off
 from collections.abc import Callable  # noqa: F401
-from typing import (Any, Generic, Iterable, Sequence,  # noqa: F401, UP035
-                    Tuple, TypeVar)
+from typing import Any, Generic, Iterable, Sequence, Tuple, TypeVar  # noqa: F401
 
 from jax import Array  # noqa: F401
 
--- setup.py
+++
@@ -1,5 +0,0 @@
-"""Setup. Required for the installation in editable mode."""
-
-from setuptools import setup
-
-setup()
