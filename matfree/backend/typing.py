88be7e64cfd35783872c6b8cfeda341c1302e81f
Nicholas Krämer <nicokraemer@gmx.net>
2023-10-11 09:14:57 +0200

    Fixed a linter-complaint

--- matfree/backend/typing.py
+++ matfree/backend/typing.py
@@ -1,7 +1,7 @@
 """Types."""
 # fmt: off
 from collections.abc import Callable  # noqa: F401
-from typing import Any, Generic, Iterable, Sequence, Tuple, TypeVar  # noqa: F401
+from typing import Any, Generic, Iterable, Sequence, Tuple, TypeVar  # noqa: F401, UP035
 
 from jax import Array  # noqa: F401
 
