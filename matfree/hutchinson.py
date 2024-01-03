259ee84a414074e3da3faaf568ce43da0a2ed659
Nicholas Krämer <nicokraemer@gmx.net>
2024-01-03 13:09:43 +0100

    Removed an outdated todo (#169)

--- matfree/hutchinson.py
+++ matfree/hutchinson.py
@@ -2,10 +2,6 @@
 
 from matfree.backend import func, linalg, np, prng, tree_util
 
-# todo: allow a fun() that returns pytrees instead of arrays.
-#  why? Because then we rival trace_and_variance as
-#  trace_and_frobeniusnorm(): y=Ax; return (x@y, y@y)
-
 
 def hutchinson(integrand_fun, /, sample_fun):
     """Construct Hutchinson's estimator.
