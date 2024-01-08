6b9224ef6a17a8a543db802209770c63cb4e3f68
Nicholas Krämer <nicokraemer@gmx.net>
2024-01-08 14:18:18 +0100

    Rename classical gram-schmidt function

--- tests/test_decomp/test_gram_schmidt.py
+++ tests/test_decomp/test_gram_schmidt.py
@@ -5,7 +5,7 @@ def qr_via_gs_c(M):
     Q0 = np.zeros_like(M)
     R0 = np.zeros_like(M)
     for i in range(len(M)):
-        v, n, c = gram_schmidt_orthogonalise_set(M[:, i], Q0.T)
+        v, n, c = gram_schmidt_classical_set(M[:, i], Q0.T)
         Q0 = Q0.at[:, i].set(v / linalg.vector_norm(v))
         R0 = R0.at[:, i].set(c)
         R0 = R0.at[i, i].set(n)
@@ -17,21 +17,18 @@ def positive_diag(q, r):
     return q * signs[None, :], signs[:, None] * r
 
 
-def gram_schmidt_orthogonalise_set(vec, vectors):  # Gram-Schmidt
-    (vec, _x), coeffs = control_flow.scan(
-        gram_schmidt_orthogonalise, (vec, vec), xs=vectors
-    )
+def gram_schmidt_classical_set(vec, vectors):  # Gram-Schmidt
+    vec, coeffs = control_flow.scan(gram_schmidt_classical, vec, xs=vectors)
     return vec / linalg.vector_norm(vec), linalg.vector_norm(vec), coeffs
 
 
-def gram_schmidt_orthogonalise(x, vec2):
-    vec1, x1 = x
-    coeff = linalg.vecdot(x1, vec2)
+def gram_schmidt_classical(vec1, vec2):
+    coeff = linalg.vecdot(vec1, vec2)
     vec_ortho = vec1 - coeff * vec2
-    return (vec_ortho, x1), coeff
+    return vec_ortho, coeff
 
 
-def test_sth():
+def test_manual_qr_matches_numpy_qr():
     n = 3
     key = prng.prng_key(2)
     key1, key2 = prng.split(key, 2)
@@ -39,7 +36,5 @@ def test_sth():
     M = prng.normal(key2, shape=(n, n))
     Q, R = positive_diag(*linalg.qr_reduced(M))
     Q_, R_ = positive_diag(*qr_via_gs_c(M))
-    print(Q, Q_)
-    print(R, R_)
     assert np.allclose(R, R_)
     assert np.allclose(Q, Q_)
