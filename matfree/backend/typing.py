39930f19353d6f26d33649c8341628838eefe089
Nicholas Krämer <nicokraemer@gmx.net>
2024-01-08 11:18:32 +0100

    Clarify typing in matfree.decomp

--- matfree/backend/typing.py
+++ matfree/backend/typing.py
@@ -1,7 +1,15 @@
 """Types."""
 # fmt: off
 from collections.abc import Callable  # noqa: F401
-from typing import Any, Generic, Iterable, Sequence, Tuple, TypeVar  # noqa: F401, UP035
+from typing import (  # noqa: F401, UP035
+    Any,
+    Generic,
+    Iterable,
+    Sequence,
+    Tuple,
+    TypeAlias,
+    TypeVar,
+)
 
 from jax import Array  # noqa: F401
 
