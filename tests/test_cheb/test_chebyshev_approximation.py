a68c993526f41452563b737d3cf4aa49e7fee0d2
Nicholas Krämer <nicokraemer@gmx.net>
2024-01-12 11:55:38 +0100

    Tighten the value-test for Chebyshev interpolation

--- tests/test_cheb/test_chebyshev_approximation.py
+++ tests/test_cheb/test_chebyshev_approximation.py
@@ -19,19 +19,14 @@ def chebyshev_approximate(fun, nodes, x):
     t2_n, t1_n = chebyshev_init(nodes)
     c1 = np.mean(fx_nodes * t1_n)
     c2 = 2 * np.mean(fx_nodes * t2_n)
-    print()
-    print(0, c1)
-    print(1, c2)
+
     t2_x, t1_x = chebyshev_init(x)
     value = c1 * t1_x + c2 * t2_x
     for i in range(len(nodes)):
         t2_n, t1_n = chebyshev_step(nodes, (t2_n, t1_n))
         c2 = 2 * np.mean(fx_nodes * t2_n)
-        print(i + 2, c2)
-
         t2_x, t1_x = chebyshev_step(x, (t2_x, t1_x))
         value += c2 * t2_x
-    print()
     return value
 
 
@@ -42,18 +37,14 @@ def chebyshev_nodes(n, /):
 
 def test_interpolate_function():
     def fun(x):
-        return np.sin(x)
+        return x**5
 
     x0 = np.linspace(-0.9, 0.9, num=5, endpoint=True)
 
-    nodes = chebyshev_nodes(10)
-    # print(nodes)
-    # t1, t0 = chebyshev_init(nodes)
-    # c0 = np.mean(fun(nodes) * t0)
-    # c1 = 2*np.mean(fun(nodes) * t1)
+    nodes = chebyshev_nodes(7)
 
     approximate = chebyshev_approximate(fun, nodes, x0)
-    print("Approximation\t", approximate)
-    print("Solution\t", fun(x0))
-    # print("Ration\t", approximate / fun(x0))
+
+    assert np.allclose(approximate, fun(x0), atol=1e-6)
+
     assert False
