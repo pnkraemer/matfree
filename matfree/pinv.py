5eb719f2ab2ba16430cd3ca634aaf517698c0e4d
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-24 13:47:34 +0200

    Deprecate matfree.pinv (#186)

--- matfree/pinv.py
+++ matfree/pinv.py
@@ -1,6 +1,18 @@
 """Pseudo-inverses of linear operators."""
 
+import warnings
+
 from matfree.backend.typing import Callable
+
+
+def _warn_deprecated():
+    msg = "The module matfree.pinv has been deprecated and will be removed soon. "
+    msg += "The removal will happen either in v0.0.17 or in v0.1.0, "
+    msg += "depending on what comes first. "
+    msg += "If your code relies on matfree.pinv, create an issue *now*."
+
+    warnings.warn(msg, DeprecationWarning, stacklevel=1)
+
 
 # todo: what happens if we want to reuse e.g. LU decompositions of AA^*?
 # todo: should we not just return pinv-matvec but also pinv-vecmat?
@@ -28,6 +40,7 @@ def pinv_tall(Av: Callable, vA: Callable, *, solve: Callable) -> Callable:
         Matrix-vector product function that implements
         the pseudo-inverse of the matrix-vector product function `Av`.
     """
+    _warn_deprecated()
 
     def pinv(s):
         return vA(solve(lambda v: Av(vA(v)), s))
@@ -57,6 +70,7 @@ def pinv_wide(Av: Callable, vA: Callable, *, solve: Callable) -> Callable:
         Matrix-vector product function that implements
         the pseudo-inverse of the matrix-vector product function `Av`.
     """
+    _warn_deprecated()
 
     def pinv(s):
         return solve(lambda v: vA(Av(v)), vA(s))
