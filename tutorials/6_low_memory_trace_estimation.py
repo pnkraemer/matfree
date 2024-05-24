f1cb7c6944901c0d4b82596b9179179dcbc83f34
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-24 11:34:11 +0200

    Implement a tutorial for low-memory trace-estimation (#184)
    * Implement a tutorial for low-memory trace estimation
    
    * Update one of the linter settings

---
+++ tutorials/6_low_memory_trace_estimation.py
@@ -0,0 +1,79 @@
+"""Carry out stochastic trace estimation with minimal memory.
+
+Matfree's implementation of stochastic trace estimation
+via Hutchinson's method defaults to computing all
+Monte-Carlo samples at once, because this is the fastest
+implementation as long as all samples fit into memory.
+
+
+Some matrix-vector products, however, are so large that
+we can only store a single sample in memory at once.
+Here is how we can wrap calls around the trace estimators
+in such a scenario to save memory.
+"""
+
+import jax
+import jax.numpy as jnp
+
+from matfree import hutchinson
+
+# ## Stochastic trace estimation
+#
+# The conventional setup for estimating the trace of a large matrix
+# would look like this.
+
+nrows = 100  # but imagine nrows=100,000,000,000 instead
+nsamples = 1_000
+
+
+def large_matvec(v):
+    return 1.2345 * v
+
+
+integrand = hutchinson.integrand_trace(large_matvec)
+
+x0 = jnp.ones((nrows,))
+sampler = hutchinson.sampler_rademacher(x0, num=nsamples)
+estimate = hutchinson.hutchinson(integrand, sampler)
+
+key = jax.random.PRNGKey(1)
+trace = estimate(key)
+print(trace)
+
+
+# The above code requires nrows*nsamples storage, which
+# is prohibitive for extremely large matrices.
+# Instead, we can loop around estimate() to do the following:
+# The below code requires nrows*1 storage:
+
+sampler = hutchinson.sampler_rademacher(x0, num=1)
+estimate = hutchinson.hutchinson(integrand, sampler)
+
+key = jax.random.PRNGKey(2)
+keys = jax.random.split(key, num=nsamples)
+traces = jax.lax.map(estimate, keys)
+trace = jnp.mean(traces)
+print(trace)
+
+# In practice, we often combine both approaches by choosing
+# the largest nsamples (in the first implementation) so that
+# nrows*nsamples fits into memory, and handle all samples beyond
+# that via the split-and-map combination.
+#
+#
+# If we reverse-mode differentiate through the sampler, we have to
+# be careful because by default, reverse-mode differentiation
+# stores all intermediate results (and the memory-efficiency of using
+# jax.lax.map is void).
+# To solve this problem, place a jax.checkpoint around the estimator:
+
+traces = jax.lax.map(jax.checkpoint(estimate), keys)
+trace = jnp.mean(traces)
+print(trace)
+
+# This implementation recomputes the forward pass for each key during the
+# backward pass, but preserves the memory-efficiency on the backward pass.
+#
+#
+# In summary, memory efficiency can be achieved by calling estimators
+# inside jax.lax.map (with or without checkpoints).
