7662e2839e46f545871372590d01693d6b6f9f20
Nicholas Krämer <nicokraemer@gmx.net>
2024-05-28 08:40:16 +0200

    Move bidiagonalisation to the bottom of the file so that Arnoldi and Lanczos are next to each other

--- matfree/decomp.py
+++ matfree/decomp.py
@@ -295,146 +295,6 @@ def _tridiag_adjoint_step(
     return (xs_all, xi, lambda_), (gradient_increment, lambda_, mu, nu, xi)
 
 
-def bidiag(
-    Av: Callable, vA: Callable, depth, /, matrix_shape, validate_unit_2_norm=False
-):
-    """Construct an implementation of **bidiagonalisation**.
-
-    Uses pre-allocation and full reorthogonalisation.
-
-    Works for **arbitrary matrices**. No symmetry required.
-
-    Decompose a matrix into a product of orthogonal-**bidiagonal**-orthogonal matrices.
-    Use this algorithm for approximate **singular value** decompositions.
-    """
-    nrows, ncols = matrix_shape
-    max_depth = min(nrows, ncols) - 1
-    if depth > max_depth or depth < 0:
-        msg1 = f"Depth {depth} exceeds the matrix' dimensions. "
-        msg2 = f"Expected: 0 <= depth <= min(nrows, ncols) - 1 = {max_depth} "
-        msg3 = f"for a matrix with shape {matrix_shape}."
-        raise ValueError(msg1 + msg2 + msg3)
-
-    class State(containers.NamedTuple):
-        i: int
-        Us: Array
-        Vs: Array
-        alphas: Array
-        betas: Array
-        beta: Array
-        vk: Array
-
-    def init(init_vec: Array) -> State:
-        if validate_unit_2_norm:
-            init_vec = _validate_unit_2_norm(init_vec)
-
-        alphas = np.zeros((depth + 1,))
-        betas = np.zeros((depth + 1,))
-        Us = np.zeros((depth + 1, nrows))
-        Vs = np.zeros((depth + 1, ncols))
-        v0, _ = _normalise(init_vec)
-        return State(0, Us, Vs, alphas, betas, np.zeros(()), v0)
-
-    def apply(state: State, *parameters) -> State:
-        i, Us, Vs, alphas, betas, beta, vk = state
-        Vs = Vs.at[i].set(vk)
-        betas = betas.at[i].set(beta)
-
-        uk = Av(vk, *parameters) - beta * Us[i - 1]
-        uk, alpha = _normalise(uk)
-        uk, *_ = _gram_schmidt_classical(uk, Us)  # full reorthogonalisation
-        Us = Us.at[i].set(uk)
-        alphas = alphas.at[i].set(alpha)
-
-        vk = vA(uk, *parameters) - alpha * vk
-        vk, beta = _normalise(vk)
-        vk, *_ = _gram_schmidt_classical(vk, Vs)  # full reorthogonalisation
-
-        return State(i + 1, Us, Vs, alphas, betas, beta, vk)
-
-    def extract(state: State, /):
-        _, uk_all, vk_all, alphas, betas, beta, vk = state
-        return uk_all.T, (alphas, betas[1:]), vk_all, (beta, vk)
-
-    alg = _LanczosAlg(
-        init=init, step=apply, extract=extract, lower_upper=(0, depth + 1)
-    )
-    return func.partial(_decompose_fori_loop, algorithm=alg)
-
-
-def _validate_unit_2_norm(v, /):
-    # todo: replace this functionality with normalising internally.
-    #
-    # Lanczos assumes a unit-2-norm vector as an input
-    # We cannot raise an error based on values of the init_vec,
-    # but we can make it obvious that the result is unusable.
-    is_not_normalized = np.abs(linalg.vector_norm(v) - 1.0) > 10 * np.finfo_eps(v.dtype)
-    return control_flow.cond(
-        is_not_normalized, lambda s: np.nan() * np.ones_like(s), lambda s: s, v
-    )
-
-
-def _gram_schmidt_classical(vec, vectors):  # Gram-Schmidt
-    vec, coeffs = control_flow.scan(_gram_schmidt_classical_step, vec, xs=vectors)
-    vec, length = _normalise(vec)
-    return vec, length, coeffs
-
-
-def _gram_schmidt_classical_step(vec1, vec2):
-    coeff = linalg.inner(vec1, vec2)
-    vec_ortho = vec1 - coeff * vec2
-    return vec_ortho, coeff
-
-
-def _normalise(vec):
-    length = linalg.vector_norm(vec)
-    return vec / length, length
-
-
-def _decompose_fori_loop(v0, *parameters, algorithm: _LanczosAlg):
-    r"""Decompose a matrix purely based on matvec-products with A.
-
-    The behaviour of this function is equivalent to
-
-    ```python
-    def decompose(v0, *matvec_funs, algorithm):
-        init, step, extract, (lower, upper) = algorithm
-        state = init(v0)
-        for _ in range(lower, upper):
-            state = step(state, *matvec_funs)
-        return extract(state)
-    ```
-
-    but the implementation uses JAX' fori_loop.
-    """
-    init, step, extract, (lower, upper) = algorithm
-    init_val = init(v0)
-
-    def body_fun(_, s):
-        return step(s, *parameters)
-
-    result = control_flow.fori_loop(lower, upper, body_fun=body_fun, init_val=init_val)
-    return extract(result)
-
-
-def _bidiagonal_dense(d, e):
-    diag = linalg.diagonal_matrix(d)
-    offdiag = linalg.diagonal_matrix(e, 1)
-    return diag + offdiag
-
-
-def _eigh_tridiag_sym(diag, off_diag):
-    # todo: once jax supports eigh_tridiagonal(eigvals_only=False),
-    #  use it here. Until then: an eigen-decomposition of size (order + 1)
-    #  does not hurt too much...
-    diag = linalg.diagonal_matrix(diag)
-    offdiag1 = linalg.diagonal_matrix(off_diag, -1)
-    offdiag2 = linalg.diagonal_matrix(off_diag, 1)
-    dense_matrix = diag + offdiag1 + offdiag2
-    eigvals, eigvecs = linalg.eigh(dense_matrix)
-    return eigvals, eigvecs
-
-
 def hessenberg(
     matvec,
     krylov_depth,
@@ -680,3 +540,143 @@ def _hessenberg_adjoint_step(
 def _extract_diag(x, offset=0):
     diag = linalg.diagonal(x, offset=offset)
     return linalg.diagonal_matrix(diag, offset=offset)
+
+
+def bidiag(
+    Av: Callable, vA: Callable, depth, /, matrix_shape, validate_unit_2_norm=False
+):
+    """Construct an implementation of **bidiagonalisation**.
+
+    Uses pre-allocation and full reorthogonalisation.
+
+    Works for **arbitrary matrices**. No symmetry required.
+
+    Decompose a matrix into a product of orthogonal-**bidiagonal**-orthogonal matrices.
+    Use this algorithm for approximate **singular value** decompositions.
+    """
+    nrows, ncols = matrix_shape
+    max_depth = min(nrows, ncols) - 1
+    if depth > max_depth or depth < 0:
+        msg1 = f"Depth {depth} exceeds the matrix' dimensions. "
+        msg2 = f"Expected: 0 <= depth <= min(nrows, ncols) - 1 = {max_depth} "
+        msg3 = f"for a matrix with shape {matrix_shape}."
+        raise ValueError(msg1 + msg2 + msg3)
+
+    class State(containers.NamedTuple):
+        i: int
+        Us: Array
+        Vs: Array
+        alphas: Array
+        betas: Array
+        beta: Array
+        vk: Array
+
+    def init(init_vec: Array) -> State:
+        if validate_unit_2_norm:
+            init_vec = _validate_unit_2_norm(init_vec)
+
+        alphas = np.zeros((depth + 1,))
+        betas = np.zeros((depth + 1,))
+        Us = np.zeros((depth + 1, nrows))
+        Vs = np.zeros((depth + 1, ncols))
+        v0, _ = _normalise(init_vec)
+        return State(0, Us, Vs, alphas, betas, np.zeros(()), v0)
+
+    def apply(state: State, *parameters) -> State:
+        i, Us, Vs, alphas, betas, beta, vk = state
+        Vs = Vs.at[i].set(vk)
+        betas = betas.at[i].set(beta)
+
+        uk = Av(vk, *parameters) - beta * Us[i - 1]
+        uk, alpha = _normalise(uk)
+        uk, *_ = _gram_schmidt_classical(uk, Us)  # full reorthogonalisation
+        Us = Us.at[i].set(uk)
+        alphas = alphas.at[i].set(alpha)
+
+        vk = vA(uk, *parameters) - alpha * vk
+        vk, beta = _normalise(vk)
+        vk, *_ = _gram_schmidt_classical(vk, Vs)  # full reorthogonalisation
+
+        return State(i + 1, Us, Vs, alphas, betas, beta, vk)
+
+    def extract(state: State, /):
+        _, uk_all, vk_all, alphas, betas, beta, vk = state
+        return uk_all.T, (alphas, betas[1:]), vk_all, (beta, vk)
+
+    alg = _LanczosAlg(
+        init=init, step=apply, extract=extract, lower_upper=(0, depth + 1)
+    )
+    return func.partial(_decompose_fori_loop, algorithm=alg)
+
+
+def _validate_unit_2_norm(v, /):
+    # todo: replace this functionality with normalising internally.
+    #
+    # Lanczos assumes a unit-2-norm vector as an input
+    # We cannot raise an error based on values of the init_vec,
+    # but we can make it obvious that the result is unusable.
+    is_not_normalized = np.abs(linalg.vector_norm(v) - 1.0) > 10 * np.finfo_eps(v.dtype)
+    return control_flow.cond(
+        is_not_normalized, lambda s: np.nan() * np.ones_like(s), lambda s: s, v
+    )
+
+
+def _gram_schmidt_classical(vec, vectors):  # Gram-Schmidt
+    vec, coeffs = control_flow.scan(_gram_schmidt_classical_step, vec, xs=vectors)
+    vec, length = _normalise(vec)
+    return vec, length, coeffs
+
+
+def _gram_schmidt_classical_step(vec1, vec2):
+    coeff = linalg.inner(vec1, vec2)
+    vec_ortho = vec1 - coeff * vec2
+    return vec_ortho, coeff
+
+
+def _normalise(vec):
+    length = linalg.vector_norm(vec)
+    return vec / length, length
+
+
+def _decompose_fori_loop(v0, *parameters, algorithm: _LanczosAlg):
+    r"""Decompose a matrix purely based on matvec-products with A.
+
+    The behaviour of this function is equivalent to
+
+    ```python
+    def decompose(v0, *matvec_funs, algorithm):
+        init, step, extract, (lower, upper) = algorithm
+        state = init(v0)
+        for _ in range(lower, upper):
+            state = step(state, *matvec_funs)
+        return extract(state)
+    ```
+
+    but the implementation uses JAX' fori_loop.
+    """
+    init, step, extract, (lower, upper) = algorithm
+    init_val = init(v0)
+
+    def body_fun(_, s):
+        return step(s, *parameters)
+
+    result = control_flow.fori_loop(lower, upper, body_fun=body_fun, init_val=init_val)
+    return extract(result)
+
+
+def _bidiagonal_dense(d, e):
+    diag = linalg.diagonal_matrix(d)
+    offdiag = linalg.diagonal_matrix(e, 1)
+    return diag + offdiag
+
+
+def _eigh_tridiag_sym(diag, off_diag):
+    # todo: once jax supports eigh_tridiagonal(eigvals_only=False),
+    #  use it here. Until then: an eigen-decomposition of size (order + 1)
+    #  does not hurt too much...
+    diag = linalg.diagonal_matrix(diag)
+    offdiag1 = linalg.diagonal_matrix(off_diag, -1)
+    offdiag2 = linalg.diagonal_matrix(off_diag, 1)
+    dense_matrix = diag + offdiag1 + offdiag2
+    eigvals, eigvecs = linalg.eigh(dense_matrix)
+    return eigvals, eigvecs
