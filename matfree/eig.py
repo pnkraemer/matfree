562ab360f7a04f44f078684daefd48188cfaf9b4
Nicholas Krämer <nicokraemer@gmx.net>
2025-01-08 11:17:11 +0100

    Change ordering of the returned vecs in eig.eig_partial

--- matfree/eig.py
+++ matfree/eig.py
@@ -1,6 +1,6 @@
 """Matrix-free eigenvalue and singular-value analysis."""
 
-from matfree.backend import linalg, np
+from matfree.backend import linalg
 from matfree.backend.typing import Array, Callable
 
 
@@ -82,12 +82,6 @@ def eig_partial(hessenberg: Callable) -> Callable:
         # Compute SVD of factorisation
         vals, vecs = linalg.eig(H)
         vecs = Q @ vecs
-
-        # Return in descending order
-        # (jnp.linalg.eig doesn't do this by default)
-        args = np.argsort(vals)[::-1]
-        vals = vals[args]
-        vecs = vecs[:, args]
-        return vals, vecs
+        return vals, vecs.T
 
     return eig
