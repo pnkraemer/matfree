f64f901ffd06acb8aceb28d879a6d775debff21b
Nicholas Krämer <nicokraemer@gmx.net>
2025-01-08 12:03:56 +0100

    Make the partial SVD compatible with Pytrees (#236)
    * Make eig.svd_partial compatible with Pytrees
    
    * Improve the documentation of the partial-SVD tests
    
    * Delete a redundant test

--- tests/test_eig/test_svd_partial.py
+++ tests/test_eig/test_svd_partial.py
@@ -32,12 +32,7 @@ def test_equal_to_linalg_svd(nrows, ncols):
 @testing.parametrize("nrows", [10])
 @testing.parametrize("ncols", [3])
 @testing.parametrize("num_matvecs", [0, 2, 3])
-def test_shapes_as_expected(nrows, ncols, num_matvecs):
-    """The output of full-depth SVD should be equal (*) to linalg.svd().
-
-    (*) Note: The singular values should be identical,
-    and the orthogonal matrices should be orthogonal. They are not unique.
-    """
+def test_shapes_as_expected_vectors(nrows, ncols, num_matvecs):
     d = np.arange(1.0, 1.0 + min(nrows, ncols))
     A = test_util.asymmetric_matrix_from_singular_values(d, nrows=nrows, ncols=ncols)
     v0 = np.ones((ncols,))
@@ -49,3 +44,26 @@ def test_shapes_as_expected(nrows, ncols, num_matvecs):
     assert Ut.shape == (num_matvecs, nrows)
     assert S.shape == (num_matvecs,)
     assert Vt.shape == (num_matvecs, ncols)
+
+
+@testing.parametrize("nrows", [10])
+@testing.parametrize("num_matvecs", [0, 2, 3])
+def test_shapes_as_expected_lists_tuples(nrows, num_matvecs):
+    K = np.arange(1.0, 10.0).reshape((3, 3))
+    v0 = np.ones((nrows, nrows))  # tensor-valued input
+
+    # Map Pytrees to Pytrees
+    def Av(v: tuple, stencil) -> list:
+        (x,) = v
+        return [np.convolve2d(stencil, x)]
+
+    bidiag = decomp.bidiag(num_matvecs)
+    svd = eig.svd_partial(bidiag)
+    [Ut], S, (Vt,) = svd(Av, (v0,), K)
+
+    # Ut inherits pytree-shape from the outputs (list),
+    # and Vt inherits pytree-shape from the inputs (tuple)
+    [u0] = Av((v0,), K)
+    assert Ut.shape == (num_matvecs, *u0.shape)
+    assert S.shape == (num_matvecs,)
+    assert Vt.shape == (num_matvecs, *v0.shape)
