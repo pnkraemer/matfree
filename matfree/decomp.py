a0ee4f88578f83c09ade2c437c7b53aad0240991
Theo <12211068+Whatisthisname@users.noreply.github.com>
2025-08-02 07:09:13 +0200

    Rename variable to adhere to paper (#248)
    * Correct variable name in loop.
    
    * Rename "eta" to "gamma" to adhere to paper
    
    * Re-rename from dH_k to dh_k.

--- matfree/decomp.py
+++ matfree/decomp.py
@@ -472,14 +472,14 @@ def _hessenberg_adjoint(matvec, *params, Q, H, r, c, dQ, dH, dr, dc, reortho: st
     lower_mask = lower(np.ones((num_matvecs, num_matvecs)))
 
     # Initialise
-    eta = dH @ e_K - Q.T @ dr
-    lambda_k = dr + Q @ eta
+    gamma = dH @ e_K - Q.T @ dr
+    lambda_k = dr + Q @ gamma
     Lambda = np.zeros_like(Q)
     Gamma = np.zeros_like(dQ.T @ Q)
     dp = tree.tree_map(np.zeros_like, params)
 
     # Prepare more  auxiliary matrices
-    Pi_xi = dQ.T + linalg.outer(eta, r)
+    Pi_xi = dQ.T + linalg.outer(gamma, r)
     Pi_gamma = -dc * c * linalg.outer(e_1, e_1) + H @ dH.T - (dQ.T @ Q)
 
     # Prepare reorthogonalisation:
@@ -498,7 +498,7 @@ def _hessenberg_adjoint(matvec, *params, Q, H, r, c, dQ, dH, dr, dc, reortho: st
         "lower_mask": lower_mask,
         "Pi_gamma": Pi_gamma,
         "Pi_xi": Pi_xi,
-        "dH_k": dH.T,
+        "dh_k": dH.T,
         "reortho_mask_k": reortho_mask,
         "q": Q.T,
     }
@@ -543,7 +543,7 @@ def _hessenberg_adjoint_step(
     Pi_xi,
     q,
     # Loop over: reorthogonalisation
-    dH_k,
+    dh_k,
     reortho_mask_k,
     # Other parameters
     Q,
@@ -553,11 +553,11 @@ def _hessenberg_adjoint_step(
     if reortho == "full":
         # Get rid of the (I_ll o Sigma) term by multiplying with a mask
         Q_masked = reortho_mask_k[None, :] * Q
-        rhs_masked = reortho_mask_k * dH_k
+        rhs_masked = reortho_mask_k * dh_k
 
         # Project x to Q^T x = y via
         # x = x - Q Q^\top x + Q Q^\top x = x - Q Q^\top x + Q y
-        # (here, x = lambda_k and y = dH_k)
+        # (here, x = lambda_k and y = dh_k)
         lambda_k = lambda_k - Q_masked @ (Q_masked.T @ lambda_k) + Q_masked @ rhs_masked
 
     # Transposed matvec and parameter-gradient in a single matvec
