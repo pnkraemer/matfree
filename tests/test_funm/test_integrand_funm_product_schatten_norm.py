cb65586c7dca9d945e95eab0380f7c83b4d36dc3
Nicholas Krämer <nicokraemer@gmx.net>
2025-11-03 09:14:19 +0100

    Fix the tests

--- tests/test_funm/test_integrand_funm_product_schatten_norm.py
+++ tests/test_funm/test_integrand_funm_product_schatten_norm.py
@@ -17,7 +17,7 @@ def make_A(nrows, ncols, num_significant_singular_vals):
 @testing.parametrize("ncols", [30])
 @testing.parametrize("num_significant_singular_vals", [30])
 @testing.parametrize("num_matvecs", [20])
-@testing.parametrize("power", [1, 2, 5])
+@testing.parametrize("power", [1, 2, 3])
 def test_schatten_norm(nrows, ncols, num_significant_singular_vals, num_matvecs, power):
     """Assert that the Schatten norm is accurate."""
     A = make_A(nrows, ncols, num_significant_singular_vals)
@@ -35,4 +35,4 @@ def test_schatten_norm(nrows, ncols, num_significant_singular_vals, num_matvecs,
     received = estimate(lambda v: A @ v, key)
 
     print_if_assert_fails = ("error", np.abs(received - expected), "target:", expected)
-    assert np.allclose(received, expected, atol=1e-2, rtol=1e-2), print_if_assert_fails
+    assert np.allclose(received, expected, atol=0.01, rtol=0.01), print_if_assert_fails
