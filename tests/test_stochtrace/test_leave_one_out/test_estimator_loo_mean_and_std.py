d1ae18fedb7eb17627731d426136be2975b5884a
Nicholas Krämer <nicokraemer@gmx.net>
2026-06-08 11:24:28 +0200

    Remove stochtrace.monte_carlo_wrap_moments and replace it with a dedicated error estimation API (#276)
    * Remove stochtrace.monte_carlo_wrap_moments and replace it with estimator_*_mean_and_std functions
    
    * Improve formatting

---
+++ tests/test_stochtrace/test_leave_one_out/test_estimator_loo_mean_and_std.py
@@ -0,0 +1,48 @@
+"""Test estimator_leave_one_out_mean_and_std."""
+
+import jax.numpy as jnp
+
+from matfree import stochtrace
+from matfree.backend import np, prng, testing
+
+
+@testing.parametrize("seed", [1, 2, 3])
+def test_mean_matches_estimator_leave_one_out(seed):
+    """Assert that the mean equals the result of estimator_leave_one_out."""
+    A = jnp.reshape(jnp.arange(16.0), (4, 4)) / 16 + jnp.eye(4)
+
+    def matvec(x):
+        return A @ x
+
+    x_like = jnp.ones((4,))
+    key = prng.prng_key(seed)
+    sampler = stochtrace.sampler_normal(x_like, num=4)
+    integrand = stochtrace.leave_one_out_xtrace()
+
+    estimate_plain = stochtrace.estimator_leave_one_out(integrand, sampler=sampler)
+    mean_only = estimate_plain(matvec, key)
+    estimate = stochtrace.estimator_leave_one_out_mean_and_std(
+        integrand, sampler=sampler
+    )
+    mean, _std = estimate(matvec, key)
+    assert np.allclose(mean, mean_only)
+
+
+@testing.parametrize("seed", [1, 2, 3])
+def test_std_error_is_nonnegative(seed):
+    """Assert that std_error is non-negative."""
+    A = jnp.reshape(jnp.arange(25.0), (5, 5)) / 25 + jnp.eye(5)
+
+    def matvec(x):
+        return A @ x
+
+    x_like = jnp.ones((5,))
+    key = prng.prng_key(seed)
+    sampler = stochtrace.sampler_normal(x_like, num=5)
+    integrand = stochtrace.leave_one_out_xtrace()
+
+    estimate = stochtrace.estimator_leave_one_out_mean_and_std(
+        integrand, sampler=sampler
+    )
+    _mean, std_error = estimate(matvec, key)
+    assert std_error >= 0.0
